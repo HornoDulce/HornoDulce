176690c0a42802f3230d79858a902fbf8dbb50a1
HornoDulce <hornodulce045@gmail.com>
2025-07-31 01:12:25 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Aquí podrías controlar dinámicamente la disponibilidad con JS en el futuro
+  // Por ahora, edita manualmente el texto en cada .status
+});
